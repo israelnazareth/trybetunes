03dd95d1886a2589c83abc8a33cfea870a97630a
Israel <israel.fab@hotmail.com>
2021-09-14 17:18:53 -0300

    Requisito 1

---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ProfileEdit extends Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">Edit Profile</div>
+    );
+  }
+}
+
+export default ProfileEdit;
