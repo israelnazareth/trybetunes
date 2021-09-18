2ee220f56f4a15ebc305f12a655ea3f961556942
Israel <israel.fab@hotmail.com>
2021-09-18 16:42:57 -0300

    Requisito 8

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      checked: false,
+    };
+  }
+
+  handleChange = async ({ target: { id, checked } }) => {
+    this.setState({
+      loading: true,
+    });
+    if (checked) {
+      await addSong(id);
+      this.setState({
+        loading: false,
+        checked: true,
+      });
+    } else {
+      await removeSong(id);
+      this.setState({
+        loading: false,
+        checked: false,
+      });
+    }
+  }
+
   render() {
-    const { musicsFromURL: { previewUrl, trackName } } = this.props;
-    return (
+    const { musicsFromURL: { previewUrl, trackName, trackId } } = this.props;
+    const { loading, checked } = this.state;
+    const sectionPage = (
       <section>
         <p>{ trackName }</p>
         <audio data-testid="audio-component" src={ previewUrl } controls>
@@ -12,7 +43,23 @@ class MusicCard extends Component {
           O seu navegador não suporta o elemento
           <code>audio</code>
         </audio>
+        <label
+          htmlFor={ trackId }
+          data-testid={ `checkbox-music-${trackId}` }
+        >
+          Favorita
+          <input
+            type="checkbox"
+            id={ trackId }
+            checked={ checked }
+            onChange={ this.handleChange }
+          />
+        </label>
       </section>
+    );
+
+    return (
+      loading ? <Loading /> : sectionPage
     );
   }
 }
@@ -21,6 +68,7 @@ MusicCard.propTypes = {
   musicsFromURL: PropTypes.shape({
     previewUrl: PropTypes.string,
     trackName: PropTypes.string,
+    trackId: PropTypes.number,
   }).isRequired,
 };
 
